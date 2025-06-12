07e5a47d46a3e05a5b6a3611149df3428eea45ba
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-12 09:40:08 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  default     = "us-east-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
