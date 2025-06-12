eca3e017b160ba7fa27d3b5dfaefd03a113bee97
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-12 09:52:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -107,3 +107,22 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_db_subnet_group" "default" {
+  name       = "rds-subnet-group"
+  subnet_ids = var.private_subnet_ids
+}
+
+resource "aws_db_instance" "default" {
+  identifier              = "webapp-db"
+  engine                  = "mysql"
+  instance_class          = var.db_instance_class
+  allocated_storage       = 20
+  name                    = var.db_name
+  username                = var.db_username
+  password                = var.db_password
+  db_subnet_group_name    = aws_db_subnet_group.default.name
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+  skip_final_snapshot     = true
+  publicly_accessible     = false
+  multi_az                = false
+}
