46ec1f06965bdb11ffb8f988127269fc28b87676
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-12 10:05:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -120,7 +120,7 @@ resource "aws_db_instance" "default" {
   name                    = var.db_name
   username                = var.db_username
   password                = var.db_password
-  db_subnet_group_name    = aws_subnet.private[*].id
+  db_subnet_group_name    = aws_db_subnet_group.default.name
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   skip_final_snapshot     = true
   publicly_accessible     = false
