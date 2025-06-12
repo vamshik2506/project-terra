7802ca211ad1b24b4cad9650406906618ee49f33
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-12 09:50:24 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,3 +17,11 @@ variable "private_subnet_cidrs" {
   type        = list(string)
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
+variable "ami_id" {}
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
