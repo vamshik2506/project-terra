a408470ad5a07e34252041fce9b46e2d6ba9cd57
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-12 09:41:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
-provider "aws" {
-    region = "us-east-2"  # Set your desired AWS region
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count      = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
 }
